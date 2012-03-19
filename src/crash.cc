ddb969222266755eadc41c9ce9b2d050d412fe30
nulltask <nulltask@gmail.com>
2012-03-19 23:27:24 +0900

    initial commit

---
+++ src/crash.cc
@@ -0,0 +1,18 @@
+#include <node.h>
+#include <v8.h>
+
+using namespace v8;
+
+Handle<Value>
+Method(const Arguments& args) {
+  HandleScope scope;
+  delete 0xffffffff;
+  return scope.Close(String::New("Unreachable!"));
+}
+
+extern "C" void
+init(Handle<Object> target) {
+  target->Set(String::NewSymbol("crash"),
+    FunctionTemplate::New(Method)->GetFunction());
+}
+NODE_MODULE(crash, init)
