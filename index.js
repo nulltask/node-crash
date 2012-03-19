722623beb211ea1946043e142cdfefce1ef9ea92
nulltask <nulltask@gmail.com>
2012-03-20 00:01:57 +0900

    expose version

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+
+/**
+ * Expose module.
+ */
+
 module.exports = require('./build/Release/crash');
+
+/**
+ * Import package information.
+ */
+
+var package = require('./package');
+
+/**
+ * Expose version.
+ */
+
+module.exports.version = package.version;
+
